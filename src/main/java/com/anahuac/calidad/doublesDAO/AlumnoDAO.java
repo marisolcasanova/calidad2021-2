0f96153e1ab146621770c64f8c91571d2b1af3e8
marisolcasanova <marisolcasanova14@gmail.com>
2021-10-12 21:15:31 -0500

    doubles DAO

---
+++ src/main/java/com/anahuac/calidad/doublesDAO/AlumnoDAO.java
@@ -0,0 +1,9 @@
+package com.anahuac.calidad.doublesDAO;
+
+public interface AlumnoDAO {
+	public void addAlumno(Alumno a);
+	public void deleteAlumno(Alumno a);
+	public void updateEmail(Alumno a);
+	public Alumno consultarAlumno(String id);
+
+}
---
+++ src/main/java/com/anahuac/calidad/doublesDAO/FakeOracleDAO.java
@@ -0,0 +1,31 @@
+package com.anahuac.calidad.doublesDAO;
+
+public class FakeOracleDAO implements AlumnoDAO {
+
+	@Override
+	public void addAlumno(Alumno a) {
+		// TODO Auto-generated method stub
+		//return false;
+	
+
+	}
+
+	@Override
+	public void deleteAlumno(Alumno a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void updateEmail(Alumno a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Alumno consultarAlumno(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
